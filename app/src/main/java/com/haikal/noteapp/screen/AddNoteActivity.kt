fd56b72e8dc20ca8cdd067e596b43e16bdc9148c
Ahmad Haikal <haikala0010@gmail.com>
2024-11-14 14:15:06 +0700

    create noteapp

---
+++ app/src/main/java/com/haikal/noteapp/screen/AddNoteActivity.kt
@@ -0,0 +1,43 @@
+package com.haikal.noteapp.screen
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.haikal.noteapp.R
+import com.haikal.noteapp.databinding.ActivityAddNoteBinding
+import com.haikal.noteapp.helper.NoteDatabaseHelper
+import com.haikal.noteapp.model.ModelNote
+
+class AddNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var db: NoteDatabaseHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NoteDatabaseHelper(this)
+
+        binding.saveButton.setOnClickListener {
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditText.text.toString()
+            val note = ModelNote(0, title, content)
+            db.insertNote(note)
+            finish()
+            Toast.makeText(this, "Catatan disimpan", Toast.LENGTH_SHORT).show()
+        }
+
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_add_note)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+}
